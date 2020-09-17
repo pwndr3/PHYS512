c08871b752d108e71a803eeac5789a2a9dfc1c3c
pwndr3 <andre.vallieres@mail.mcgill.ca>
2020-09-17 00:25:38 -0400

    Problems 1 & 2 completed.

---
+++ assignments/hw1/prob1_b.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def deriv(fun, x, delta):
+    """
+    Returns derivative of function `fun` evaluated at point `x`
+    with the smallest step being `delta`.
+    """
+    f1 = (fun(x + delta) - fun(x - delta))/(2 * delta) # Step of delta
+    f2 = (fun(x + 2 * delta) - fun(x - 2 * delta))/(4 * delta) # Step of 2 delta
+    return (f2 - 4 * f1) / (-3)
+
+if __name__ == '__main__':
+    # Set up delta to sweep
+    n = np.linspace(-16, -2, 40)
+    delta_i = 10 ** n
+
+    # Initialize arrays to plot
+    errors = np.zeros(len(delta_i))
+    
+    # 1) exp(x)
+    x0 = 1
+    truth = np.exp(x0)
+    for i, delta in enumerate(delta_i):
+        est = deriv(np.exp, x0, delta)
+        errors[i] = np.abs(est - truth)
+    print("Optimal delta: %s" % delta_i[np.argmin(errors)])
+    
+    # 2) exp(0.01x)
+    def exp_001(x):
+        return np.exp(0.01 * x)
+    
+    x0 = 1
+    truth = 0.01 * exp_001(x0)
+    for i, delta in enumerate(delta_i):
+        est = deriv(exp_001, x0, delta)
+        errors[i] = np.abs(est - truth)
+    print("Optimal delta: %s" % delta_i[np.argmin(errors)])
---
+++ assignments/hw1/prob2.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import interpolate
+
+if __name__ == '__main__':
+    # Load data (T, V)
+    data = np.loadtxt('lakeshore.txt', usecols=(0,1))
+
+    # Sort to have increasing voltage
+    data = data[data[:,1].argsort()]
+    
+    # Interpolate with odd points
+    interp = interpolate.PchipInterpolator(data[::2,1], data[::2,0])
+    
+    # Plot
+    plt.plot(data[:,1], data[:,0])
+    plt.plot(data[:,1], interp(data[:,1]))
+    plt.xlabel('Voltage (V)')
+    plt.ylabel('Temperature (K)')
+    plt.legend(['Real data', 'Interpolated data'])
+    plt.savefig('lakeshore_interp.png')
+    
+    # Compute error on even points
+    print("Error: %f" % np.std(data[1::2,0] - interp(data[1::2,1])))
